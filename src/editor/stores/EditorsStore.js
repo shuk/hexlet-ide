c8a3588ed5b38ab9cd03fab99fe9455ee33e2958
alexeyfrank <frank@undev.ru>
2014-09-18 15:14:52 +0400

    refactor editor tabs

---
+++ src/editor/stores/EditorsStore.js
@@ -0,0 +1,78 @@
+/* global require module */
+var _ = require("lodash");
+
+var AppDispatcher = require("editor/dispatcher/AppDispatcher");
+var BaseStore = require("./BaseStore");
+var ActionTypes = require("editor/constants/IdeConstants").ActionTypes;
+
+var editors = [];
+
+var EditorsStore = BaseStore.extend({
+  getAll: function() {
+    return editors;
+  },
+
+  getCurrent: function() {
+    return _.find(editors, { current: true });
+  }
+});
+
+AppDispatcher.registerHandler(ActionTypes.TREE_OPEN_FILE, function(payload) {
+  editors.map(function(t) { t.current = false; });
+
+  var item = payload.item;
+  var content = payload.content;
+
+  var editor = _.find(editors, {id: item.id});
+  if (!editor) {
+    editors.push({id: item.id, dirty: false, name: item.name, current: true, content: content});
+  } else {
+    editor.current = true;
+  }
+
+  EditorsStore.emitChange();
+});
+
+AppDispatcher.registerHandler(ActionTypes.EDITORS_EDIT_CURRENT, function(payload) {
+  var editor = _.find(editors, {id: payload.id});
+  editor.content = payload.content;
+  editor.dirty = true;
+
+  EditorsStore.emitChange();
+});
+
+AppDispatcher.registerHandler(ActionTypes.EDITORS_SAVE_CURRENT, function(payload) {
+  var editor = _.find(editors, {id: payload.id});
+  editor.dirty = false;
+
+  EditorsStore.emitChange();
+});
+
+AppDispatcher.registerHandler(ActionTypes.EDITORS_MAKE_CURRENT, function(payload) {
+  editors.map(function(t) { t.current = false; });
+
+  var editor = _.find(editor, {id: payload.id});
+  editor.current = true;
+
+  EditorsStore.emitChange();
+});
+
+AppDispatcher.registerHandler(ActionTypes.EDITORS_CLOSE, function(payload) {
+  editors = _.filter(editors, function(t) { return t.id !== payload.id; });
+
+  if (editors.length > 0) {
+    var editor = _.last(editors);
+    editor.current = true;
+  }
+
+  EditorsStore.emitChange();
+});
+
+// AppDispatcher.registerHandler(ActionTypes.TABS_FLUSH_CONTENT, function(payload) {
+//   tab = tabs[payload.id];
+//   if (tab !== undefined) {
+//     tabs[payload.id].content = payload.content;
+//   }
+// });
+
+module.exports = EditorsStore;
--- test/fixtures/project/main.js
+++ test/fixtures/project/main.js
@@ -1,2 +1,3 @@
 
 
+asasdfasd 
