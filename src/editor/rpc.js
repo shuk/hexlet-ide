a0200727dcfcc1e4631a5bb94f1c23e8c834b436
alexeyfrank <frank@undev.ru>
2014-09-19 18:01:59 +0400

    remove eureca uri config

--- src/editor/rpc.js
+++ src/editor/rpc.js
@@ -1,15 +1,2 @@
 /* global module location Eureca */
-
-// var EurecaClient = require("eureca.io").EurecaClient;
-
-var uri = "http://localhost";
-if (location.origin) {
-  uri = location.origin;
-} else {
-  uri = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
-}
-
-module.exports = new Eureca.Client({
-  transport: "sockjs",
-  uri: uri
-});
+module.exports = new Eureca.Client();
