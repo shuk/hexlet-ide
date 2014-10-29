0b27e70105d9606211fefa8d63caf7af34a9fb2a
alexeyfrank <frank@undev.ru>
2014-10-29 17:25:36 +0300

    publish new version

--- bin/hexlet-ide.js
+++ bin/hexlet-ide.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
-/* global require process */
+/* global require process console */
 
+var fs = require("fs");
 var path = require("path");
 
 var program = require("commander");
 
 program
-  // .version("0.0.1")
-  .option("-p, --port [num]", "Port", process.env.PORT || 8080)
-  .option("-r, --root-dir [path]", "Root directory", process.cwd())
-  .parse(process.argv);
+.option("-p, --port [num]", "Port", process.env.PORT || 8080)
+.option("-r, --root-dir [path]", "Root directory", process.cwd())
+.parse(process.argv);
 
-  program.rootDir = path.resolve(process.env.TEST_DIR || program.rootDir);
-  console.log("info: Root dir is " + program.rootDir);
+program.rootDir = path.resolve(process.env.TEST_DIR || program.rootDir);
+if (!fs.existsSync(program.rootDir)) {
+  throw "Directory :" + program.rootDir + " is not exist!";
+}
+
+console.log("info: Root dir is " + program.rootDir);
 
 require("../src/backend/server")(program);
