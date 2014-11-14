1fd345c442eccdd7216d5529d3b40e7bd77fb34d
d.chernyatiev <d.chernyatiev@undev.ru>
2014-11-14 17:24:24 +0300

    Tests update
    Zombie removed
    Legacy tests removed

--- test/plugins/CloseTreeNode.js
+++ test/plugins/CloseTreeNode.js
@@ -5,14 +5,13 @@ var expect = require("chai").expect;
 module.exports = function(fileName) {
   return function(nightmare) {
     var selector = "[data-name='" + fileName + "'].tree-branch-name";
-
     var containerSelector = "[data-name='" + fileName + "'].tree-branch.tree-open";
+
     nightmare
-    .exists(containerSelector, function(result)  {
-      expect(result).to.equal(true);
-    })
-    .evaluate(function() { return document.querySelector("#hexlet-ide").innerHTML; }, function(res) { console.log(res); })
-    .click(selector)
-    .wait();
+      .exists(containerSelector, function(result)  {
+        expect(result).to.equal(true);
+      })
+      .click(selector)
+      .wait();
   };
 };
--- test/plugins/DefaultSettings.js
+++ test/plugins/DefaultSettings.js
@@ -3,7 +3,10 @@ var testHelper = require("../testHelper");
 module.exports = function() {
   return function(nightmare) {
     nightmare
-    .viewport(1280, 1024)
-    .goto(testHelper.baseUrl);
+      .on("consoleMessage", function(msg, line, sourceId) {
+        console.log("CONSOLE:", msg);
+      })
+      .viewport(1280, 1024)
+      .goto(testHelper.baseUrl);
   };
 };
